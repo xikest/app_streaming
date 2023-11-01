9e1ee9da64ab94236c461ba55f8d04dc63459fe2
xikest <94699546+xikest@users.noreply.github.com>
2023-11-01 12:29:03 +0900

    Update nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,6 +1,13 @@
 import nltk
 
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('averaged_perceptron_tagger')
+class NLTK_UTILS:
+
+  def __init__(self):
+    pass
+  
+  def download(self) -> None:   
+      nltk.download('punkt')
+      nltk.download('stopwords')
+      nltk.download('averaged_perceptron_tagger')
+      return None
