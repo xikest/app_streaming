6628c8d5563c7b26582c2a4f0f2480f1ba9b46f3
xikest <xikest12@gmail.com>
2023-11-02 12:18:31 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,8 +8,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-import gensim
-from gensim import corpora
 import re
 import nltk
 
