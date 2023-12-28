0967f17393ff6703c9e71047812dd9218691d735
xikest <xikest12@gmail.com>
2023-12-28 18:22:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,28 +10,31 @@ def main():
         st.session_state.messages = []
 
     apikey = st.text_input("input your GPT api key")
-    gpt_assistant = GPTAssistant(api_key=apikey)
+    try:
+        gpt_assistant = GPTAssistant(api_key=apikey)
 
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
-    if prompt := st.chat_input("What is up?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+        if prompt := st.chat_input("What is up?"):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
 
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            response_messages = [
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ]
-            full_response = gpt_assistant.generate_response(response_messages)
-            message_placeholder.markdown(full_response + "▌")
-            message_placeholder.markdown(full_response)
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
-
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                response_messages = [
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ]
+                full_response = gpt_assistant.generate_response(response_messages)
+                message_placeholder.markdown(full_response + "▌")
+                message_placeholder.markdown(full_response)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+    except:
+        if apikey is "":
+            st.error("🚨 Input your API KEY")
 
 if __name__ == "__main__":
     main()
